20843e557d5a35f24d3ad4c75d99e676cb0c2d63
bistaarina <arinabista1021@gmail.com>
2025-05-26 21:48:16 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const app = express()
+// const app = require("express")()
+
+app.listen(3000, function(){
+    console.log("Backend has started at port 3000")
+})
