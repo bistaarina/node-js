497eff9ebfd236a20c2c6b7dde09733ef47677ba
bistaarina <arinabista1021@gmail.com>
2025-05-27 18:48:29 +0545

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require("express")
 const app = express()
 // const app = require("express")()
+app.set("view engine", "ejs")//tells express js to set environmnt for ejs to run
 
+
+app.get("/", function(req, res){
+    res.render("home.ejs")
+    
+})
+app.get("/about", function(req, res){
+    res.render("about.ejs")
+})
 app.listen(3000, function(){
     console.log("Backend has started at port 3000")
 })
